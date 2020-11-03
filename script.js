ec1c95d8b7cf232bb8cbc67169c88290ec72332f
NDPenaloza <ndpenaloza@gmail.com>
2020-11-03 00:13:16 -0800

    added footer and card divs

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
 
 // DIV where seached cities will be appended
 "#search-city-list"
+
+//DIV to dump current day info
+"#current-day-weather"
+
+// 5 day forecast
+"#day1"
+
+"day2"
+
+"day3"
+
+"day4"
+
+"day5"
