71232398e7661b6403f6c94e647058a07ab2adbd
NDPenaloza <ndpenaloza@gmail.com>
2020-11-04 23:21:23 -0800

    added buildCitySearchList function

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
-// City Search Input
-"#city-search-input"
+$(document).ready(function() {
+    // Need to build the query search
+    function buildQueryURL() {
+        // City Search Input
+        var citySearchInput = $("#city-search-input").val().trim();
 
-// Search button to find city
-"#search-button"
+        var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-// DIV where seached cities will be appended
-"#search-city-list"
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + citySearchInput + APIKey;
 
-//DIV to dump current day info
-"#current-day-weather"
+        console.log()
+    }
 
-// 5 day forecast
-"#day1"
+    buildQueryURL();
 
-"day2"
+    // Search button to find city
+    $("#search-button").on("click", function(event) { 
 
-"day3"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // Store retrieved data in "response" object
+        .then(function(response) {
+            // Log queryURL
+            console.log(queryURL);
+            // Log response
+            console.log(response);
 
-"day4"
+            // Transfer content to HTML
+            
+        })
 
-"day5"
+        // Append city to search-city-list div in an unordered list button
+        function buildCitySearchList() {
+            // DIV where seached cities will be appended
+            var searchCityList = $("#search-city-list");
+            // after city is searched, it will be stored as a button
+            var citySearchedBtn = $("<button >");
+            // citySearched will be appended to searchCityList div
+            searchCityList.append(citySearchedBtn);
+
+        }
+        // Calling buildCitySearchList function 
+        buildCitySearchList();
+    })
+
+    
+
+    //DIV to dump current day info
+    "#current-day-weather"
+
+    // 5 day forecast
+    "#day1"
+
+    "day2"
+
+    "day3"
+
+    "day4"
+
+    "day5"
+
+});
